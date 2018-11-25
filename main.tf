20aa7b9a2b350a1c51180cdf1ad0ea0d54685b00
Valavan Rajakumar <valavan@ad.corp.google.com>
2018-11-24 23:02:02 -0500

    Add files:

---
+++ main.tf
@@ -0,0 +1,27 @@
+// Configure the Google Cloud provider
+provider "google" {
+  credentials = "${file("../keys/sa.json")}"
+  project     = "bnc-pso-poc"
+  region      = "us-central1"
+}
+
+// Create a new instance
+resource "google_compute_instance" "test" {
+  count        = 1 // Adjust as desired
+  name         = "test${count.index + 1}" // yields "test1", "test2", etc. It's also the machine's name and hostname
+  machine_type = "f1-micro" // smallest (CPU &amp; RAM) available instance
+  zone         = "us-central1-b" // yields "europe-west1-d" as setup previously. Places your VM in Europe
+
+ 
+boot_disk {
+initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default" 
+    access_config {
+          network_tier = "STANDARD"
+         }
+    } 
+  }
